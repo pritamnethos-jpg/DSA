a4cb8320ed8fb3a5b1853602a4bd749d3d9fe36e
Pritam nath <pritamn.ethos@gmail.com>
2026-07-21 13:58:12 +0530

    question number 12 done by me

--- Patterns/SL12.cpp
+++ Patterns/SL12.cpp
@@ -1,41 +1,22 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-// Function to print the pattern
-void pattern12(int N) {
-    // Initial number of spaces in the first row
-    int spaces = 2 * (N - 1);
-    
-    // Outer loop for the number of rows
-    for (int i = 1; i <= N; i++) {
-        
-        // Inner loop to print numbers in increasing order
-        for (int j = 1; j <= i; j++) {
-            cout << j;
+int main()
+{
+    int n=4;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
         }
-        
-        // Inner loop to print spaces in the middle
-        for (int j = 1; j <= spaces; j++) {
-            cout << " ";
+        for(int j=0;j<=2*(n-i);j++)
+        {
+            cout<<" ";
         }
-        
-        // Inner loop to print numbers in decreasing order
-        for (int j = i; j >= 1; j--) {
-            cout << j;
+        for (int j = i; j >= 1; j--)
+        {
+            cout<<j;
         }
-        
-        // Move to the next line after printing the row
-        cout << endl;
-        
-        // Decrease spaces by 2 after each row
-        spaces -= 2;
+        cout<<endl;
     }
-}
-
-int main() {
-    // Set the value of N
-    int N = 5;
-    pattern12(N); // Call the function to print the pattern
-
-    return 0;
 }
